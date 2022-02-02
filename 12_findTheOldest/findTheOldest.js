d574a5b551843166e36297813e8ccfa7303d84e2
Rodolfo <rudolfrce@gmail.com>
2022-02-02 11:18:06 -0700

    Add solution to 12 exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const actualYear = new Date().getFullYear();
+  for (let i = 0, len = people.length; i < len; i++) {
+    if (people[i].yearOfDeath === undefined) {
+      let birth = people[i].yearOfBirth;
+      people[i]["age"] = actualYear - birth;
+    } else {
+      let birth = people[i].yearOfBirth;
+      let death = people[i].yearOfDeath;
+      people[i]["age"] = death - birth;
+    }
+  }
+  oldestPerson = people.reduce((previousPerson, currentPerson) => {
+    if (previousPerson.age > currentPerson.age) {
+      return previousPerson;
+    } else return currentPerson;
+  });
+  return oldestPerson;
 };
 
 // Do not edit below this line
