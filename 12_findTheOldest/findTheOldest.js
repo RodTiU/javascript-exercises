f67fd8a49bd603037a4285736a3deb1529929d54
Rodolfo <rudolfrce@gmail.com>
2022-02-02 12:39:29 -0700

    Add solution to 12 exercise refactor with map and reduce methods

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,16 +1,15 @@
 const findTheOldest = function (people) {
   const actualYear = new Date().getFullYear();
-  for (let i = 0, len = people.length; i < len; i++) {
-    if (people[i].yearOfDeath === undefined) {
-      let birth = people[i].yearOfBirth;
-      people[i]["age"] = actualYear - birth;
+  const ageOfPeople = people.map((person) => {
+    if (person.yearOfDeath === undefined) {
+      person["age"] = actualYear - person.yearOfBirth;
+      return person;
     } else {
-      let birth = people[i].yearOfBirth;
-      let death = people[i].yearOfDeath;
-      people[i]["age"] = death - birth;
+      person["age"] = person.yearOfDeath - person.yearOfBirth;
+      return person;
     }
-  }
-  oldestPerson = people.reduce((previousPerson, currentPerson) => {
+  });
+  const oldestPerson = ageOfPeople.reduce((previousPerson, currentPerson) => {
     if (previousPerson.age > currentPerson.age) {
       return previousPerson;
     } else return currentPerson;
